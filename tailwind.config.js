02972e5a3e352708b83f7fb1040bd5f8e6034fa2
eshan1799 <eshan1799@gmail.com>
2021-02-18 17:31:31 +0000

    tailwindcss setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './pages/**/*.js',
+    './components/**/*.js'
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
