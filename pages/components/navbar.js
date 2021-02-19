dbd7c633b7107f3e34dc96ec32c3be8d6f3510a4
eshan1799 <eshan1799@gmail.com>
2021-02-19 17:16:13 +0000

    add bottom border to navbar

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function NavBar() {
     return (
-        <nav className="py-6 flex-start text-left font-bold text-white">
+        <nav className="py-6 flex-start text-left font-bold text-white border-b-2 border-gray-100">
             <Link href="/">
                 <a className="px-20">
                     Invoicing
