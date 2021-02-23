e7629035973399a8c9f3db1ce2820750eed65fc4
eshan1799 <eshan1799@gmail.com>
2021-02-23 15:25:24 +0000

    added hover effect on navlinks

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -4,12 +4,12 @@ export default function NavBar() {
     return (
         <nav className="py-6 flex-start text-left font-bold text-white border-b-2 border-gray-100">
             <Link href="/">
-                <a className="px-20">
+                <a className="px-20 hover:opacity-60">
                     Invoicing
                 </a>
             </Link>
             <Link href="/savingscalculator">
-                <a>
+                <a className="hover:opacity-60">
                     Savings Calculator
                 </a>
             </Link>
