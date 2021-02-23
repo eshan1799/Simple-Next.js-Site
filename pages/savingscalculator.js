23dfe9050ac68928b22a1d8a12820791581d341f
eshan1799 <eshan1799@gmail.com>
2021-02-23 15:46:58 +0000

    change fetch url to deployed site

--- pages/savingscalculator.js
+++ pages/savingscalculator.js
@@ -20,7 +20,7 @@ export default function SavingsCalculator() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        fetch(`http://localhost:3000/api/calculation?monthlyCost=${ state.monthlyCost }&employeeCount=${ state.employeeCount }`)
+        fetch(`https://simple-next-js-site.vercel.app/savingscalculator?monthlyCost=${ state.monthlyCost }&employeeCount=${ state.employeeCount }`)
         .then(res => res.json())
         .then((data) => {
             setState({ moneySaved: data.moneySaved })
